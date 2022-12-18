cf9396b053651abdc2a7dde684a81777f04ae5f4
Ruggero Nocera <9341160+rogierz@users.noreply.github.com>
2022-12-18 11:27:43 +0100

    some commits

--- test_random_policy.py
+++ test_random_policy.py
@@ -16,24 +16,29 @@ from env_utils import *
 
 
 def main():
-    render_mode = 'human'  # set to 'human' for rendering (does not work on Google Colab)
+    # set to 'human' for rendering (does not work on Google Colab)
+    render_mode = 'human'
 
     env = make_env(domain="source", render_mode=render_mode)
 
     print('State space:', env.observation_space)  # state-space
     print('Action space:', env.action_space)  # action-space
-    print('Dynamics parameters:', env.get_parameters())  # masses of each link of the Hopper
+    # masses of each link of the Hopper
+    print('Dynamics parameters:', env.get_parameters())
 
     n_episodes = 5
 
-    for ep in range(n_episodes):  
+    for ep in range(n_episodes):
+        print(f"Starting ep: {ep}")
         done = False
         state = env.reset()  # Reset environment to initial state
 
         while not done:  # Until the episode is over
             action = env.action_space.sample()  # Sample random action
 
-            state, reward, done, info = env.step(action)  # Step the simulator to the next timestep
+            # Step the simulator to the next timestep
+            state, reward, done, info = env.step(action)
+
 
 if __name__ == '__main__':
     main()
