d234fd72c927ced1d422b79fc86a110bfd5138dc
Riccardo Sepe <seric814@gmail.com>
2022-12-27 13:32:02 +0100

    Added command line parameters to main.py

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-# from run.steps.step3 import main as step3
+import sys
+
 from run.steps.step2a import main as step2a
+from run.steps.step3 import main as step3
 
 
-def main():
-    step2a()
+def main(args):
+
+    if len(args) > 1:
+        raise RuntimeWarning("Expected only one argument. Ignoring all the others.")
+
+    step = args[0]
+
+    if step == '--2':
+        step2a()
+    elif step == '--3':
+        # TODO: cfg parameter
+        step3('')
 
 
 if __name__ == '__main__':
-    main()
+    argv = sys.argv[1:]
+    main(argv)
