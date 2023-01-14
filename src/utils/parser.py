ba025b09c3041789bae9538b6c1cf2c0dd5f74ff
Riccardo Sepe <seric814@gmail.com>
2023-01-14 12:47:05 +0100

    Added number of optuna trials cl argument

--- src/utils/parser.py
+++ src/utils/parser.py
@@ -5,3 +5,5 @@ parser.add_argument(
     "--step", required=True, help="The number of the step to execute", type=int)
 parser.add_argument(
     "--n", help="Split of the gridsearch to execute (1, 2 or 3)", default=0, type=int)
+parser.add_argument("--t", help="The number of trials for the optimization study (suggested values: 100-1000) (default "
+                                "is 10)", default=10, type=int)
