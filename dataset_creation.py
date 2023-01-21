7f463d37ce7fee5ac9b26592e5875aa006b32724
francescoscalera99 <francesoscalera99@gmail.com>
2023-01-21 18:01:12 +0100

    updated dataset_creation.py

--- dataset_creation.py
+++ dataset_creation.py
@@ -8,11 +8,11 @@ from tqdm import trange
 
 
 def main():
-    env = gym.make('CustomHopper-source-v0')
+    env = gym.make('CustomHopper-UDR-source-v0')
     base_prefix = os.path.join("dataset")
     if not os.path.exists(os.path.join("model", "data_model.zip")):
         model = SAC("MlpPolicy", env)
-        model.learn(total_timesteps=10_000)
+        model.learn(total_timesteps=50_000, progress_bar=True)
         model.save(os.path.join("model", "data_model"))
     else:
         model = SAC.load(os.path.join("model", "data_model.zip"))
@@ -26,22 +26,27 @@ def main():
         n_images = 0
         state = env.reset()
 
-        for i in trange(10_000, desc="Dataset creation"):
-            action, _ = model.predict(state)  # Sample random action
+        for i in trange(5_000, desc="Dataset creation"):
+            if np.random.rand() > 0.7:
+                action, _ = model.predict(state)
+            else:
+                action = env.action_space.sample()
             # Step the simulator to the next timestep
             state, _, done, _ = env.step(action)
 
-            if ((i+1) % 10 == 0):  # save on csv
-
-                img_state = env.render(mode="rgb_array", width=224, height=224)
-                name_img = f"hopper-{n_images}.jpeg"
-                Image.fromarray(img_state).save(
-                    os.path.join(base_prefix, name_img))
-                dataset_writer.writerow([name_img, *state])
-                n_images += 1
-
+            #if ((i+1) % 50 == 0):  # save on csv
+            env.render()
+            # img_state = env.render(mode="rgb_array", width=224, height=224)
+            # name_img = f"hopper-{n_images}.jpeg"
+            # Image.fromarray(img_state).save(
+            #     os.path.join(base_prefix, name_img))
+            # dataset_writer.writerow([name_img, *state])
+            # n_images += 1
+        
+        
             if done:
                 state = env.reset()
+
 
 
 if __name__ == "__main__":
