06bf04a4db7248e7fb8c7f8b65fe2fdb19e29a0c
Riccardo Sepe <seric814@gmail.com>
2023-01-21 18:49:06 +0100

    Added HopperDataset class

---
+++ src/dataset/dataset.py
@@ -0,0 +1,34 @@
+import os
+
+import torch
+from torch.utils.data import Dataset
+from torchvision.io import read_image
+import pandas as pd
+
+
+class HopperDataset(Dataset):
+    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None, sliding_window=5):
+        self.img_labels = pd.read_csv(annotations_file)
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
+        self.sliding_window = sliding_window
+
+    def __len__(self):
+        return len(self.img_labels) - self.sliding_window
+
+    def __getitem__(self, idx):
+        window = []
+        for _ in range(self.sliding_window):
+            img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+            image = read_image(img_path)
+            if self.transform:
+                image = self.transform(image)
+            window.append(image)
+        label = self.img_labels.iloc[idx+self.sliding_window, 1:]
+
+        if self.target_transform:
+            label = self.target_transform(label)
+
+        label = label.to_numpy().astype(float)
+        return torch.stack(window), label
