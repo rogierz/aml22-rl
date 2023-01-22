68a74ce3a38114017f7efd9de40478e65c37acde
Riccardo Sepe <seric814@gmail.com>
2023-01-22 12:49:18 +0100

    Implementing step4

--- src/steps/step4.py
+++ src/steps/step4.py
@@ -33,7 +33,9 @@ def main(base_prefix="."):
 
     total_timesteps = int(1e5)
 
-    env = gym.make(f"CustomHopper-source-v0")
+    env = gym.make(f"CustomHopper-UDR-source-v0")
+    env_source = gym.make(f"CustomHopper-source-v0")
+    env_target = gym.make(f"CustomHopper-target-v0")
     env = ResizeObservation(CustomWrapper(
         PixelObservationWrapper(env)), shape=(128, 128))
     obs = env.reset()
@@ -41,6 +43,28 @@ def main(base_prefix="."):
     model = SAC('CnnPolicy', env, **sac_params, seed=42, buffer_size=100000)
 
     model.learn(total_timesteps=total_timesteps, progress_bar=True)
+
+    model.save("cnn_model")
+
+    n_episodes = 5_000
+
+    for env_name, test_env in [("source", env_source), ("target", env_target)]:
+        run_avg_return = 0
+        for ep in range(n_episodes):
+            done = False
+            n_steps = 0
+            obs = test_env.reset()
+            episode_return = 0
+
+            while not done:  # Until the episode is over
+                action, _ = model.predict(obs, deterministic=True)
+                obs, reward, done, info = test_env.step(action)
+                n_steps += 1
+                episode_return += reward
+                test_env.render()
+
+            run_avg_return += episode_return
+        run_avg_return /= n_episodes
 
 
 if __name__ == '__main__':
