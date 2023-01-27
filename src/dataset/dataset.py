6ab46e4627beb377f0fb7829ad9dea994ced6d98
Riccardo Sepe <seric814@gmail.com>
2023-01-27 20:12:22 +0100

    Added docstring to dataset.py

--- src/dataset/dataset.py
+++ src/dataset/dataset.py
@@ -1,13 +1,26 @@
+"""
+This file contains a torch.utils.data.Dataset subclass to use for pytorch dataloaders.
+"""
+
 import os
 
+import pandas as pd
 import torch
 from torch.utils.data import Dataset
 from torchvision.io import read_image
-import pandas as pd
 
 
 class HopperDataset(Dataset):
     def __init__(self, annotations_file, split="train", transform=None, target_transform=None, sliding_window=5):
+        """
+        HopperDataset is a subclass of torch.utils.data.Dataset.
+
+        :param annotations_file: Specify the path to the csv file which contains the mapping <filename>:<observation>
+        :param split: Specify which split of the data to use
+        :param transform: Specify the data augmentation
+        :param target_transform: Specify the transformation on the target data
+        :param sliding_window: Define the size of the sliding window used to infer the velocities
+        """
         if split not in ["train", "val", "test"]:
             raise ValueError("Split must be one of [train, val, test]")
 
