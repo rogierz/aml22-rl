0c27d9e01c038ed8402f3392bbe9be9e56a49b8f
francescoscalera99 <francesoscalera99@gmail.com>
2023-01-27 18:17:46 +0100

    added logger for step 4
    Co-authored-by: Riccardo Sepe <pastarick@users.noreply.github.com>
    Co-authored-by: Ruggero Nocera <rogierz@users.noreply.github.com>

--- src/steps/step4.py
+++ src/steps/step4.py
@@ -13,7 +13,7 @@ from gym.wrappers.pixel_observation import PixelObservationWrapper
 from gym.wrappers.resize_observation import ResizeObservation
 from stable_baselines3 import SAC
 from torch.utils.tensorboard import SummaryWriter
-
+from stable_baselines3.common.logger import configure
 from model.env.custom_hopper import *
 
 
@@ -27,6 +27,7 @@ class CustomWrapper(gym.ObservationWrapper):
 
 
 def main(base_prefix=".", force=False):
+
     logdir = f"{base_prefix}/sac_tb_step4_log"
 
     if os.path.isdir(logdir):
@@ -39,90 +40,67 @@ def main(base_prefix=".", force=False):
             print(f"Directory {logdir} already exists. Shutting down...")
             return
 
+    logger = configure(logdir, ["stdout", "tensorboard"])
+
+    
+
     sac_params = {
         "learning_rate": 2e-3,
         "gamma": 0.99,
         "batch_size": 128
     }
 
-    total_timesteps = int(1e5)
-
-    # env = gym.make(f"CustomHopper-UDR-source-v0")
-    # env_source = gym.make(f"CustomHopper-source-v0")
-    # env_target = gym.make(f"CustomHopper-target-v0")
-    # env = ResizeObservation(CustomWrapper(
-    #     PixelObservationWrapper(env)), shape=(128, 128))
-    # obs = env.reset()
-    # print(obs.shape)
-
-    # model = SAC('CnnPolicy', env, **sac_params, seed=42, buffer_size=100000)
-
-    # model.learn(total_timesteps=total_timesteps, progress_bar=True)
-
-    # model.save("cnn_model")
-
-    # n_episodes = 50
-
-    # for env_name, test_env in [("source", env_source), ("target", env_target)]:
-    #     run_avg_return = 0
-    #     for ep in range(n_episodes):
-    #         done = False
-    #         n_steps = 0
-    #         obs = test_env.reset()
-    #         episode_return = 0
-
-    #         while not done:  # Until the episode is over
-    #             action, _ = model.predict(obs, deterministic=True)
-    #             obs, reward, done, info = test_env.step(action)
-    #             n_steps += 1
-    #             episode_return += reward
-    #             test_env.render()
-
-    #         writer.add_scalar(f'episode_return', episode_return, ep)
-    #         print(f"episode {ep} : {episode_return:.2f}")
-    #         run_avg_return += episode_return
-    #     run_avg_return /= n_episodes
-    #     print(f"source - {env_name} : {run_avg_return:.2f} ")
+    total_timesteps = int(1e6)
 
     env = gym.make(f"CustomHopper-UDR-source-v0")
-    env_source = gym.make(f"CustomHopper-source-v0")
-    env_target = gym.make(f"CustomHopper-target-v0")
+    env_source = ResizeObservation(CustomWrapper(
+                PixelObservationWrapper(gym.make(f"CustomHopper-source-v0"))), shape=(128, 128))
+    env_target =  ResizeObservation(CustomWrapper(
+                PixelObservationWrapper(gym.make(f"CustomHopper-target-v0"))), shape=(128, 128))
 
     env = ResizeObservation(CustomWrapper(
                 PixelObservationWrapper(env)), shape=(128, 128))
     obs = env.reset()
-    print(obs.shape)
 
-    model = SAC('CnnPolicy', env, **sac_params, seed=42, buffer_size=100000, tensorboard_log=logdir)
+    model = SAC('CnnPolicy', env, **sac_params, seed=42, buffer_size=100000)
+    model.set_logger(logger)
 
     model.learn(total_timesteps=total_timesteps, progress_bar=True, tb_log_name="SAC_training_CNN")
 
+    # if os.path.isfile("step4.zip"):
+    #     try:
+    #         shutil.rmtree(logdir)
+    #     except Exception as e:
+    #         print(e)
+    #     else:
+    #         #fname += datetime.now().strftime("%Y%m%D-%H%M%S")
+    #         print(f"Directory {logdir} already exists. Shutting down...")
+    #         return
     model.save(os.path.join("trained_models", "step4"))
 
     n_episodes = 50
 
-    with SummaryWriter(log_dir=logdir) as writer:
-        for env_name, test_env in [("source", env_source), ("target", env_target)]:
-            run_avg_return = 0
-            for ep in range(n_episodes):
-                done = False
-                n_steps = 0
-                obs = test_env.reset()
-                episode_return = 0
+    for env_name, test_env in [("source", env_source), ("target", env_target)]:
+        run_avg_return = 0
+        for ep in range(n_episodes):
+            done = False
+            n_steps = 0
+            obs = test_env.reset()
+            episode_return = 0
 
-                while not done:  # Until the episode is over
-                    action, _ = model.predict(obs, deterministic=True)
-                    obs, reward, done, info = test_env.step(action)
-                    n_steps += 1
-                    episode_return += reward
-                    test_env.render()
+            while not done:  # Until the episode is over
+                action, _ = model.predict(obs, deterministic=True)
+                obs, reward, done, info = test_env.step(action)
+                n_steps += 1
+                episode_return += reward
 
-                writer.add_scalar(f'episode_return', episode_return, ep)
-                print(f"episode {ep} : {episode_return:.2f}")
-                run_avg_return += episode_return
-            run_avg_return /= n_episodes
-            print(f"source - {env_name} : {run_avg_return:.2f} ")
 
+            logger.record(f'episode_return/{env_name}', episode_return)
+            logger.dump(ep)
+            run_avg_return += episode_return
+        run_avg_return /= n_episodes
+        logger.record(f'run_avg_return/{env_name}', run_avg_return)
+        logger.dump()
 
 if __name__ == '__main__':
     main()
