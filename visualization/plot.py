16b4a33c5092fc6f46ea0424dfb7e0da9292c349
Riccardo Sepe <seric814@gmail.com>
2023-01-27 18:47:20 +0100

    Added docstring for plot.py

--- visualization/plot.py
+++ visualization/plot.py
@@ -1,3 +1,25 @@
+#!/usr/bin/env python
+"""
+This script generates the plots based on the data in the `data` folder.
+
+usage: plot.py [-h] -i INPUT_FILE (-s | -f) [-o OUTPUT_FOLDER] [-n FNAME]
+               {tpe,gs}
+
+positional arguments:
+  {tpe,gs}              What you want to plot
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -i INPUT_FILE, --input-file INPUT_FILE
+                        The csv file containing the data to plot
+  -s, --show            Show the plots in a window
+  -f, --file            Save the plots in a png file (see param --fname)
+  -o OUTPUT_FOLDER, --output-dir OUTPUT_FOLDER
+                        The output folder
+  -n FNAME, --fname FNAME
+                        The output file name
+"""
+
 from argparse import ArgumentParser
 
 from plot_GS_results import main as plot_gs
@@ -10,6 +32,13 @@ PLOTTERS = {
 
 
 def main(p_name, args):
+    """
+    The main function of this script. It retrieves the correct plotter based on the command line argument and calls it.
+
+
+    :param p_name: Determine which plotter to use
+    :param args: The arguments dictionary passed by the parser
+    """
     plotter = PLOTTERS[p_name]
     plotter(**args)
 
@@ -24,6 +53,8 @@ if __name__ == '__main__':
     group1.add_argument("-f", "--file", action="count", help="Save the plots in a png file (see param --fname)")
     parser.add_argument("-o", "--output-dir", help="The output folder", type=str, default="plots", dest="output_folder")
     parser.add_argument("-n", "--fname", help="The output file name", type=str, default="plot", dest="fname")
+
+    parser.print_help()
 
     argv = vars(parser.parse_args())
 
