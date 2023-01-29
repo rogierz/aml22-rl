36c0a728399ad67999439f8667ab6a31f3c338db
Ruggero Nocera <9341160+rogierz@users.noreply.github.com>
2023-01-29 22:29:33 +0100

    updated search space in step2_3

--- src/steps/step2_3.py
+++ src/steps/step2_3.py
@@ -72,7 +72,7 @@ def objective_fn(trial, logdir='.'):
             model = SAC('MlpPolicy', env_source, learning_rate=lr_schedule(
                 lr), batch_size=batch_size, gamma=gamma)
             model.set_logger(logger)
-            model.learn(total_timesteps=150_000, progress_bar=True)
+            model.learn(total_timesteps=100_000, progress_bar=True)
 
             model.save(os.path.join("trained_models",
                                     f"step2_3_trial_{trial.number}_env_{source}"))
@@ -132,7 +132,7 @@ def main(base_prefix='.', force=False):
             return
 
     search_space = {
-        "gamma": [0.9, 0.99],
+        "gamma": [0.99],
         "learning_rate": [1e-3, 2e-3, 5e-3],
         "batch_size": [128, 256, 512],
         "lr_schedule": ["constant", "step"]
