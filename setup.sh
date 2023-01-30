d04deedea5579976a9bc6a6d4943f7330f7f1478
Ruggero Nocera <9341160+rogierz@users.noreply.github.com>
2023-01-30 11:16:53 +0100

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ echo "This script sets up the environment for the Azure virtual machines."
 
 echo "1. Installing requirements..."
 
-apt-get install -y \
+sudo apt-get install -y \
     libgl1-mesa-dev \
     libgl1-mesa-glx \
     libglew-dev \
@@ -12,28 +12,21 @@ apt-get install -y \
     software-properties-common \
     patchelf
 
-echo "2. Cloning the repo..."
+echo "2. Creating virtualenv..."
+python -m venv .venv
+echo "export MUJOCO_PY_MUJOCO_PATH=/home/azureuser/localfiles/mujoco210" >> .venv/bin/activate
+echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/home/azureuser/localfiles/mujoco210/bin" >> .venv/bin/activate
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia" >> .venv/bin/activate
 
-git clone https://github.com/rogierz/aml22-rl
+source .venv/bin/activate
 
-
-echo "3. Installing Python requirements..."
+echo "2. Installing Python requirements..."
 pip install mujoco-py
 pip install -r aml22-rl/requirements.txt
 
 
-echo "4. Downloading MuJoCo Python bindings..."
+echo "3. Downloading MuJoCo Python bindings..."
 wget https://mujoco.org/download/mujoco210-linux-x86_64.tar.gz
 tar xf mujoco210-linux-x86_64.tar.gz
 
-
 echo "Done!"
-if [[ ! $(echo $LD_LIBRARY_PATH) ]]; then
-echo "Don't forget to set up the environment variable LD_LIBRARY_PATH !"
-echo "Example: > env MUJOCO_PY_MUJOCO_PATH=/content/mujoco210"
-fi
-
-if [[ ! $(echo $MUJOCO_PY_MUJOCO_PATH) ]]; then
-echo "Don't forget to set up the environment variable MUJOCO_PY_MUJOCO_PATH"
-echo "Example: > env LD_LIBRARY_PATH=/usr/lib64-nvidia:/content/mujoco210/bin:/usr/lib/nvidia"
-fi
