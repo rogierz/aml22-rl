3f5dfd7f585f8a82cb7788b2c65ba59db8851a12
Riccardo Sepe <seric814@gmail.com>
2023-02-01 10:02:25 +0100

    Updated copy_from_remote.sh with new IP address

--- copy_from_remote.sh
+++ copy_from_remote.sh
@@ -6,7 +6,7 @@ if [ "$IP" = "-h" ] || [ "$IP" = "" ]; then
     echo "  Francesco:"
     echo "    - CPU: 20.22.91.51" #50001
     echo "  Riccardo:"
-    echo "    - CPU: 20.85.58.182" # 50000
+    echo "    - CPU: 20.85.61.104" # 50001
     echo "    - GPU: 20.97.157.108" # 50001
     echo "  Ruggero:"
     echo "    - CPU: 20.88.107.10"
@@ -17,11 +17,11 @@ fi
 
 PORT="50000"
 
-if [ "$IP" = "20.22.91.51" ] || [ "$IP" = "20.22.152.67" ]; then
+if [ "$IP" = "20.22.91.51" ] || [ "$IP" = "20.22.152.67" ] || [ "$IP" = "20.85.61.104" ]; then
     PORT="50001"
 fi
 
-if [ "$IP" = "20.88.107.10" ] || [ "$IP" = "20.22.152.67" ] || [ "$IP" = "20.22.91.51" ] || [ "$IP" = "20.85.58.182" ] || [ "$IP" = "20.97.157.108" ]; then
+if [ "$IP" = "20.88.107.10" ] || [ "$IP" = "20.22.152.67" ] || [ "$IP" = "20.22.91.51" ] || [ "$IP" = "20.85.61.104" ] || [ "$IP" = "20.97.157.108" ]; then
     echo "$IP" is valid. Starting...
     echo "Storing in $(pwd)/remote"
     rsync --progress -vr -e "ssh -p $PORT" azureuser@"$IP":/home/azureuser/localfiles/aml22-rl/trained_models remote/
