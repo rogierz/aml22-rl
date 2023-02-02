5c2d622c6f56e637592ef2674b03640b38b159f8
Riccardo Sepe <seric814@gmail.com>
2023-02-02 17:09:57 +0100

    Minor fix for MobileNet

--- src/networks/resnet.py
+++ src/networks/resnet.py
@@ -35,7 +35,7 @@ class MobileNet(BaseFeaturesExtractor):
         This corresponds to the number of unit for the last layer.
     """
 
-    def __init__(self, observation_space: spaces.Box, features_dim: int = 512, n_frames=3, pre_train=False):
+    def __init__(self, observation_space: spaces.Box, features_dim: int = 576, n_frames=3, pre_train=False):
         super().__init__(observation_space, features_dim)
 
         # weights='IMAGENET1K_V1')
@@ -45,6 +45,7 @@ class MobileNet(BaseFeaturesExtractor):
         # self.backbone.conv1 = nn.Conv2d(n_frames, 64, 3, 1, 1, bias=False)
         # self.backbone.maxpool = nn.Identity()
         # only feature maps
+        self.backbone.classifier = nn.Identity()  # nn.Linear(in_features=512, out_features=features_dim)
         self.backbone.fc = nn.Identity()  # nn.Linear(in_features=512, out_features=features_dim)
 
     def forward(self, observations: th.Tensor) -> th.Tensor:
