b334ecefee319ae35c72f7213006eb76ab89b83f
francescoscalera99 <francesoscalera99@gmail.com>
2023-02-02 10:07:09 +0100

    added Francesco's IP

--- copy_from_remote.sh
+++ copy_from_remote.sh
@@ -21,7 +21,7 @@ if [ "$IP" = "20.22.91.51" ] || [ "$IP" = "20.22.152.67" ] || [ "$IP" = "20.85.6
     PORT="50001"
 fi
 
-if [ "$IP" = "20.88.107.10" ] || [ "$IP" = "20.22.152.67" ] || [ "$IP" = "20.22.91.51" ] || [ "$IP" = "20.85.61.104" ] || [ "$IP" = "20.97.157.108" ]; then
+if [ "$IP" = "20.88.107.10" ] || [ "$IP" = "20.22.152.67" ] || [ "$IP" = "20.22.91.51" ] || [ "$IP" = "20.85.61.104" ] || [ "$IP" = "20.97.157.108" ] || [ "$IP" = "20.96.78.251" ]; then
     echo "$IP" is valid. Starting...
     echo "Storing in $(pwd)/remote"
     rsync --progress -vr -e "ssh -p $PORT" azureuser@"$IP":/home/azureuser/localfiles/aml22-rl/trained_models remote/
