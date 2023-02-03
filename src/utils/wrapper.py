7253964072c2dfeef7bcbd1b2242a985d3d50610
Riccardo Sepe <seric814@gmail.com>
2023-02-03 17:19:42 +0100

    Added docstring for wrapper.py

--- src/utils/wrapper.py
+++ src/utils/wrapper.py
@@ -1,25 +1,49 @@
+"""
+This file holds some custom gym Wrappers
+"""
+
+from enum import Enum
+
 import gym
 import numpy as np
-from enum import Enum
 from gym.spaces import Box
 
 
-class CustomWrapper(gym.ObservationWrapper):
+class ExtractionWrapper(gym.ObservationWrapper):
+    """
+    This class extracts the output from the PixelObservationWrapper and adapts it for the ResizeObservationWrapper
+    """
     def __init__(self, env):
         super().__init__(env)
         self.observation_space = Box(
             0, 255, shape=(128, 128, 3), dtype=np.uint8)
 
     def observation(self, obs):
+        """
+        This method just returns the pixels of the input observation.
+
+        :param obs: the environment visual observation
+        :return: The pixels of the image
+        """
         return obs["pixels"]
 
 
 class RewardWrapperMode(Enum):
+    """
+    This class is an enum type used to determine which variant of the RewardWrapper to run
+
+    Attributes:
+        MINIMIZE = Give a bonus to the reward if the sampled parameters are close to the target ones
+        MAXIMIZE = Give a bonus to the reward if the sampled parameters are distant from the target ones
+    """
     MINIMIZE = 0
     MAXIMIZE = 1
 
 
 class RewardWrapper(gym.RewardWrapper):
+    """
+    This class is a wrapper used to increase or decrease the rewards based on the values of :param mode
+    """
     def __init__(self, env, mode=RewardWrapperMode.MAXIMIZE, target=None):
         super().__init__(env)
         if mode not in list(RewardWrapperMode):
@@ -31,6 +55,9 @@ class RewardWrapper(gym.RewardWrapper):
         self.target = env if target is None else target
 
     def reward(self, reward):
+        """
+        Override the default reward method: the default value is scaled based on the value of self.mode.
+        """
         coeff = 1
 
         # get the value of the masses of the target environment (always not modified, even in UDR scenario)
@@ -43,12 +70,12 @@ class RewardWrapper(gym.RewardWrapper):
         similarity = (source_masses.T @ target_masses) / (np.linalg.norm(source_masses) * np.linalg.norm(target_masses))
         if self.mode == RewardWrapperMode.MAXIMIZE:
             # bonus if masses very different
-            coeff += (1-similarity)
+            coeff += (1 - similarity)
         else:
             # bonus if masses very similar
             coeff += similarity
 
-        new_reward = coeff*reward
+        new_reward = coeff * reward
         if coeff < 0 or reward < 0 or new_reward < 0:
             print(
                 f"DEBUG: found coeff={float(coeff):.2f} and reward={float(reward):.2f}, new reward is {float(new_reward):.2f}")
