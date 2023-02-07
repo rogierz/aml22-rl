ab82f8e0d9a6fcd6e9fd27ea0ff7076a7a31c6e5
Riccardo Sepe <seric814@gmail.com>
2023-02-07 18:33:14 +0100

    Fixed plot_step4_1.py

--- visualization/plotters/plot_step4_1.py
+++ visualization/plotters/plot_step4_1.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import os 
+import os
+
 
 def main():
     plt.rcParams.update({'font.size': 8})
@@ -27,7 +28,7 @@ def main():
     ax.set_title("Masses of target \n reduced by 50% (except torso)", fontsize=8)
 
     fig.savefig(os.path.join("..", "plots", "plot_step4_1_0.png"), dpi=200)
-    return 
+    return
     fig,ax = plt.subplots()
     df_step4_1_maximizeCNN = pd.read_csv(os.path.join("..", "data", "step4_1", "step4_1_1_test_MAXIMIZE_CNN_log.csv"))
     df_step4_1_maximizeMLP = pd.read_csv(os.path.join("..", "data", "step4_1", "step4_1_1_test_MAXIMIZE_MLP_log.csv"))
@@ -42,10 +43,9 @@ def main():
     ax[1].plot(x, y2, 'g')
     ax[1].plot(x, y3, 'b')
     ax[1].plot(x, y4, 'y')
-    ax[1].set_xlabel("Episode number", fontsize = 8)
-    ax[1].set_ylabel("Reward", fontsize = 8)
-    ax[1].set_title("Masses of target \n reduced by 25% (except torso)", fontsize=8)
-
+    ax[1].set_xlabel("Episode number", fontsize=8)
+    # ax[1].set_ylabel("Reward", fontsize=8)
+    # ax[1].set_title("Masses of target \n reduced by 25% (except torso)", fontsize=8)
 
     df_step4_1_maximizeCNN = pd.read_csv(os.path.join("..", "data", "step4_1", "step4_1_2_test_MAXIMIZE_CNN_log.csv"))
     df_step4_1_maximizeMLP = pd.read_csv(os.path.join("..", "data", "step4_1", "step4_1_2_test_MAXIMIZE_MLP_log.csv"))
@@ -77,9 +77,9 @@ def main():
     ax[3].plot(x, y2, 'g')
     ax[3].plot(x, y3, 'b')
     ax[3].plot(x, y4, 'y')
-    ax[3].set_xlabel("Episode number", fontsize = 8)
-    ax[3].set_ylabel("Reward", fontsize = 8)
-    ax[3].set_title("Masses of target \n increased by 25% (except torso)", fontsize=8)
+    ax[3].set_xlabel("Episode number", fontsize=8)
+    # ax[3].set_ylabel("Reward", fontsize=8)
+    # ax[3].set_title("Masses of target \n increased by 25% (except torso)", fontsize=8)
 
     df_step4_1_maximizeCNN = pd.read_csv(os.path.join("..", "data", "step4_1", "step4_1_4_test_MAXIMIZE_CNN_log.csv"))
     df_step4_1_maximizeMLP = pd.read_csv(os.path.join("..", "data", "step4_1", "step4_1_4_test_MAXIMIZE_MLP_log.csv"))
@@ -94,17 +94,28 @@ def main():
     ax[4].plot(x, y2, 'g')
     ax[4].plot(x, y3, 'b')
     ax[4].plot(x, y4, 'y')
-    ax[4].set_xlabel("Episode number", fontsize = 8)
-    ax[4].set_ylabel("Reward", fontsize = 8)
-    ax[4].set_title("Masses of target \n increased by 50% (except torso)", fontsize=8)
-
+    ax[4].set_xlabel("Episode number", fontsize=8)
+    # ax[4].set_ylabel("Reward", fontsize=8)
+    # ax[4].set_title("Masses of target \n increased by 50% (except torso)", fontsize=8)
 
     fig.legend(ncol=4, loc='upper center')
-    fig.tight_layout()
     fig.set_size_inches(10, 2)
-    plt.subplots_adjust(wspace=0.5, hspace=0.5, bottom=0.2, left=0.05)
+    # plt.subplots_adjust(wspace=0.5, hspace=0.1, left=0.07, bottom=0.13)
+    fig.tight_layout()
+    plt.subplots_adjust(top=0.8)
     fig.savefig(os.path.join("..", "plots", "plot_step4_1.png"), dpi=200)
 
 
 if __name__ == "__main__":
+    plt.rcParams.update({
+        # "text.usetex": True,
+        "font.family": "sans-serif",
+        # "font.sans-serif": ["Computer Modern Serif"],
+        "axes.titlesize": 20,
+        "xtick.labelsize": 15,
+        "ytick.labelsize": 15,
+        "axes.labelsize": 18,
+        # "legend.fontsize": 18,
+        "figure.dpi": 180
+    })
     main()
